6e3c3f74b9e3c948da11025fdef1f57e882bc97c
JAFFAR NGAIRA <garsolinajaffar@gmail.com>
2023-03-29 15:45:38 +0300

    Done

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  if (someValue >= 400) {
+  return"This is on me!";
+  }
+  else if (someValue > 2000)
+  {return 'I will gladlytake your thirty bucks.'};
+  else if (someValue < 2500)
+  {return "No can do."}
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+  if (destination==="NYC"){
+    return "Ok , sounds good"
+    if ("destination" !=="NYC"){
+      return "No go"
+    }
+  }
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  if (tip === "generous") {
+    return "Thank you so much"
+  }
+  else if (tip ==="not as generous" ){
+    return "Thank you"}
+    else{{Bye}}  
 }
